623807b08f323d7643c15e407a9e7fa00c79809c
rakaalrian <muhammad.alrian@fast-8.com>
2017-10-23 13:39:23 +0700

    first commit

---
+++ tests/unit/SampleTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+* 
+*/
+class SampleTest extends \PHPUnit\Framework\TestCase
+{
+	public function testTrueAssertsToTrue(){
+		$this->assertTrue(true);
+	}
+}
+
+?>
---
+++ tests/unit/UserTest.php
@@ -0,0 +1,70 @@
+<?php 
+
+/**
+* 
+*/
+class UserTest extends \PHPUnit\Framework\TestCase
+{
+
+	public function testGetTheFirstName(){
+		$user = new \App\Models\User;
+
+		$user->setFirstName('Billy');
+
+		$this->assertEquals($user->getFirstName(), 'Billy');
+	}
+
+	public function testGetTheLastName(){
+		$user = new \App\Models\User;
+
+		$user->setLastName('Garret');
+
+		$this->assertEquals($user->getLastName(), 'Garret');
+	}
+
+	public function testGetTheFullName(){
+		$user = new \App\Models\User;
+
+		$user->setFirstName('Billy');
+		$user->setLastName('Garret');
+
+		$this->assertEquals($user->getFullName(), 'Billy Garret');
+	}
+
+	public function testGetFirstAndLastNameAreTrimmed(){
+		$user = new \App\Models\User;
+
+		$user->setFirstName('  Billy     ');
+		$user->setLastName('  Garret ');
+
+		$this->assertEquals($user->getFirstName(), 'Billy');
+		$this->assertEquals($user->getLastName(), 'Garret');
+	}
+
+	public function testToSetEmailAddress(){
+		$user = new \App\Models\User;
+
+		$user->setEmail('billy@mail.com');
+
+		$this->assertEquals($user->getEmail(), 'billy@mail.com');
+	}
+
+	public function testGetArrayContainFullNameAndEmail(){
+		$user = new \App\Models\User;
+
+		$user->setFirstName('  Billy     ');
+		$user->setLastName('  Garret ');
+		$user->setEmail('billy@mail.com');
+
+		$getArrayData = $user->getArrayData();
+
+		$this->assertArrayHasKey('full_name', $getArrayData);
+		$this->assertArrayHasKey('email', $getArrayData);
+
+		$this->assertEquals($getArrayData['full_name'], 'Billy Garret');	
+		$this->assertEquals($getArrayData['email'], 'billy@mail.com');
+	}
+
+}
+
+?>
